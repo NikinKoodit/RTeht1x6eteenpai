e49a62b693541b821db421306555ef79ca930ca0
NikinKoodit <niki.miettinen@awacademy.training>
2022-08-04 13:39:55 +0300

    teht 1.10 button komponentti

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// tehtävä 1.9
+// tehtävä 1.10
 
 import { useState } from 'react'
 
@@ -11,50 +11,75 @@ const App = () => {
  
   return (
     <div>
-      <h1>give feedBack</h1>
+      <h1>Give feedback</h1>
 
-      <button onClick={() => setGood(good + 1)}>
-        good
-      </button>
+      <Button handleClick={()=> setGood (good +1)} text='good' />
+      <Button handleClick={()=> setNeutral (neutral +1)} text='neutral' />
+      <Button handleClick={()=> setBad (bad +1)} text='bad' />
 
-      <button onClick={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-
-      <button onClick={() => setBad(bad + 1)}>
-        bad
-      </button>
       <Statistics good= {good} neutral= {neutral} bad= {bad}/>
       
     </div>
   )
 }
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 
 const Statistics = (props) => {
+  
+
   if (props.good + props.neutral + props.bad === 0) {
     return(
       <div>
-        <h1>statistics</h1>
-        <p> No feedBack given </p>
+        <h1>Statistics</h1>
+        <h3> No feedback given </h3>
       </div>
     )
   }
-  
+ 
   return(
 
     <div>
-    <h1>statistics</h1>
-    <p>good {props.good}</p>
-    <p>neutral {props.neutral}</p>
-    <p>bad {props.bad}</p>
+      <h1>Statistics</h1>
+      <StatisticLine text="good" value ={props.good} />
+
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
   
-    <p>all {props.good + props.neutral + props.bad}</p>
-    <p>average {((props.good*1) + (props.neutral*0) - props.bad) / (props.good + props.neutral + props.bad)}</p>
-    <p>positive {props.good / (props.good + props.neutral + props.bad) * 100 } %</p>
+      <p>all {props.good + props.neutral + props.bad}</p>
+      <p>average {((props.good*1) + (props.neutral*0) - props.bad) / (props.good + props.neutral + props.bad)}</p>
+      <p>positive {props.good / (props.good + props.neutral + props.bad) * 100 } %</p>
+    
     </div>
   )
 
 }
+
+const StatisticLine = (props) => {
+  console.log(props)
+  
+  return (
+    <div>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
+  
+      <p>all {props.good + props.neutral + props.bad}</p>
+      <p>average {((props.good*1) + (props.neutral*0) - props.bad) / (props.good + props.neutral + props.bad)}</p>
+      <p>positive {props.good / (props.good + props.neutral + props.bad) * 100 } %</p>
+   
+    </div>
+
+  )
+ 
+}
+
+
 
 export default App
